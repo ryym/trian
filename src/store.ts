48c555979e91627c7d86cbbba350f5983b555bff
ryym <ryym.64@gmail.com>
2020-08-04 09:23:21 +0900

    feat(store): Handle Selector key to fetch computed value

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,16 @@
 import { Block } from './block';
+import { Selector, AsyncSelector, StoreKey, GetResult } from './selector';
 
 export interface BlockState<T> {
   current: T;
   subscribers: SetValue<T>[];
+}
+
+export interface SelectorState<T> {
+  cache: { value: T } | null;
+  updating: { depsChanged: boolean } | null;
+  invalidationHandlers: Array<() => void>;
+  dependencies: Array<{ unsubscribe: Unsubscribe }>;
 }
 
 export type SetValue<T> = (value: T) => void;
@@ -13,7 +21,13 @@ export type Unsubscribe = () => void;
 
 export class Store<BlockCtx> {
   private readonly blockStates: Map<Block<any>, BlockState<any>> = new Map();
+
   private readonly blockContext: BlockCtx;
+
+  private readonly selectorStates: Map<
+    Selector<any> | AsyncSelector<any>,
+    SelectorState<any>
+  > = new Map();
 
   constructor(blockContext: BlockCtx) {
     this.blockContext = blockContext;
@@ -28,6 +42,7 @@ export class Store<BlockCtx> {
     return state;
   }
 
+  // TODO: Rename method to onBlockValueChange.
   subscribe = <T>(block: Block<T>, setValue: SetValue<T>): Unsubscribe => {
     const state = this.getBlockState(block);
     state.subscribers.push(setValue);
@@ -41,8 +56,107 @@ export class Store<BlockCtx> {
     return unsubscribe;
   };
 
+  // TODO: Remove method in favor of selectValue.
   getValue = <T>(block: Block<T>): T => {
     return this.getBlockState(block).current;
+  };
+
+  selectValue = <K extends StoreKey<any>>(key: K): GetResult<K> => {
+    if (key instanceof Block) {
+      return this.getBlockValue(key);
+    } else if (key instanceof Selector) {
+      return this.getSelectorValue(key as any);
+    } else if (key instanceof AsyncSelector) {
+      return this.getAsyncSelectorValue(key as any) as GetResult<K>;
+    } else {
+      throw new Error(`[trian] Invalid store key: ${key}`);
+    }
+  };
+
+  private getBlockValue = <T>(block: Block<T>): T => {
+    return this.getBlockState(block).current;
+  };
+
+  private getSelectorValue = <T>(selector: Selector<T>): T => {
+    const state = this.getSelectorState(selector);
+    if (state.cache) {
+      return state.cache.value;
+    }
+
+    state.dependencies.forEach((d) => d.unsubscribe());
+    state.dependencies = [];
+
+    const invalidateCache = () => {
+      if (state.cache != null) {
+        state.cache = null;
+        state.invalidationHandlers.forEach((h) => h());
+      }
+    };
+
+    const get = <U>(key: Block<U> | Selector<U>): U => {
+      const unsubscribe = this.onInvalidate(key, invalidateCache);
+      state.dependencies.push({ unsubscribe });
+      return this.selectValue(key);
+    };
+
+    const value = selector.run({ get });
+    state.cache = { value };
+    return state.cache.value;
+  };
+
+  private getAsyncSelectorValue = async <T>(selector: AsyncSelector<T>): Promise<T> => {
+    const state = this.getSelectorState(selector);
+    if (state.cache) {
+      return Promise.resolve(state.cache.value);
+    }
+
+    state.dependencies.forEach((d) => d.unsubscribe());
+    state.dependencies = [];
+
+    const invalidateCache = () => {
+      if (state.cache != null) {
+        state.cache = null;
+        state.invalidationHandlers.forEach((h) => h());
+      }
+      if (state.updating != null) {
+        state.updating.depsChanged = true;
+      }
+    };
+
+    const get = <K extends StoreKey<any>>(key: K): GetResult<K> => {
+      const unsubscribe = this.onInvalidate(key, invalidateCache);
+      state.dependencies.push({ unsubscribe });
+      return this.selectValue(key);
+    };
+
+    state.updating = { depsChanged: false };
+    const value = await selector.run({ get });
+    if (!state.updating.depsChanged) {
+      state.cache = { value };
+    }
+    state.updating = null;
+    return value;
+  };
+
+  private getSelectorState<T>(selector: Selector<T> | AsyncSelector<T>): SelectorState<T> {
+    let state = this.selectorStates.get(selector);
+    if (state == null) {
+      state = { cache: null, updating: null, invalidationHandlers: [], dependencies: [] };
+      this.selectorStates.set(selector, state);
+    }
+    return state;
+  }
+
+  onInvalidate = (key: StoreKey<any>, handler: () => void): Unsubscribe => {
+    if (key instanceof Block) {
+      return this.subscribe(key, handler);
+    } else {
+      const state = this.getSelectorState(key);
+      state.invalidationHandlers.push(handler);
+      return function unsubscribe() {
+        state.invalidationHandlers = state.invalidationHandlers.filter((f) => f !== handler);
+      };
+    }
   };
 
   setValue = <T>(block: Block<T>, value: T): void => {
