7188793115c265a095d1364a7806b0d34aba1f0d
ryym <ryym.64@gmail.com>
2020-08-08 09:26:25 +0900

    feat: Provide basic shallow-equal utils

---
+++ src/comparer.ts
@@ -0,0 +1,42 @@
+// Implementation reference: https://github.com/moroshko/shallow-equal
+
+export type AnyObject = {
+  [key: string]: any;
+  [key: number]: any;
+};
+
+export const shallowEqualObjects = (a: AnyObject, b: AnyObject): boolean => {
+  if (a === b) {
+    return true;
+  }
+
+  const aKeys = Object.keys(a);
+  if (aKeys.length !== Object.keys(b).length) {
+    return false;
+  }
+
+  for (var i = 0; i < aKeys.length; i++) {
+    const key = aKeys[i];
+    if (a[key] !== b[key] || !Object.prototype.hasOwnProperty.call(b, key)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const shallowEqualArrays = (a: any[], b: any[]): boolean => {
+  if (a === b) {
+    return true;
+  }
+
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+};
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ export * from './dispatch';
 export * from './selector';
 export * from './store';
 
+// TODO: Separate import path from core.
+export * from './comparer';
+
+// TODO: Separate import path from core.
 export * from './react-hooks';
