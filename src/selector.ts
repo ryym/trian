4edf1ed61c549175b39b836c0da69e93a4bc2fea
ryym <ryym.64@gmail.com>
2022-06-11 17:12:57 +0900

    feat(selector): Remove AsyncSelector

--- src/selector.ts
+++ src/selector.ts
@@ -1,7 +1,5 @@
 import { Block, Comparer } from "./block";
 
-export type AnySelector<T> = Selector<T> | AsyncSelector<T>;
-
 export type Get = <T>(selector: Block<T> | Selector<T>) => T;
 
 export interface SelectorFnParams {
@@ -46,67 +44,6 @@ export class Selector<T> {
   }
 }
 
-export type AnyGet = <K extends AnyGetKey<any>>(selector: K) => AnyGetResult<K>;
-
-export type AnyGetKey<T> = Block<T> | Selector<T> | AsyncSelector<T>;
-
-export type AnyGetResult<K extends AnyGetKey<any>> = K extends AsyncSelector<infer T>
-  ? Promise<T>
-  : K extends AnyGetKey<infer T>
-  ? T
-  : never;
-
-export interface AsyncSelectorFnParams {
-  readonly get: AnyGet;
-}
-
-export type AsyncSelectorFn<T> = (params: AsyncSelectorFnParams) => Promise<T>;
-
-export interface AsyncSelectorConfig<T> {
-  readonly name?: string;
-  readonly get: AsyncSelectorFn<T>;
-  readonly isSame?: Comparer<T>;
-  readonly onCacheInvalidate?: (event: SelectorCacheInvalidateEvent<T>) => void;
-  readonly onDelete?: (event: SelectorDeletionEvent<T>) => void;
-}
-
-export class AsyncSelector<T> {
-  readonly name?: string;
-  private readonly fn: AsyncSelectorFn<T>;
-  readonly isSame: Comparer<T>;
-  readonly onCacheInvalidate?: (event: SelectorCacheInvalidateEvent<T>) => void;
-  readonly onDelete?: (event: SelectorDeletionEvent<T>) => void;
-
-  constructor(config: AsyncSelectorConfig<T>) {
-    this.name = config.name;
-    this.fn = config.get;
-    this.isSame = config.isSame || Object.is;
-    this.onCacheInvalidate = config.onCacheInvalidate;
-    this.onDelete = config.onDelete;
-  }
-
-  run(params: AsyncSelectorFnParams): Promise<T> {
-    return this.fn(params);
-  }
-}
-
-export interface NewSelector {
-  <T>(config: SelectorConfig<T>): T extends Promise<any> ? never : Selector<T>;
-  async<T>(config: AsyncSelectorConfig<T>): AsyncSelector<T>;
-}
-
-const createSelectorCreator = (): NewSelector => {
-  const selector = (<T>(config: SelectorConfig<T>): Selector<T> => {
-    return new Selector(config);
-  }) as NewSelector;
-
-  const asyncSelector = <T>(config: AsyncSelectorConfig<T>): AsyncSelector<T> => {
-    return new AsyncSelector(config);
-  };
-
-  selector.async = asyncSelector;
-
-  return selector;
+export const selector = <T>(config: SelectorConfig<T>): Selector<T> => {
+  return new Selector(config);
 };
-
-export const selector = createSelectorCreator();
--- src/selectorFamily.ts
+++ src/selectorFamily.ts
@@ -1,66 +1,27 @@
 import { Family } from "./family";
-import { Selector, AsyncSelector, SelectorConfig, AsyncSelectorConfig, selector } from "./selector";
+import { Selector, SelectorConfig, selector } from "./selector";
 
 export interface SelectorFamilyConfig<T, Args extends unknown[]> {
   readonly key: (...args: Args) => any;
   readonly selector: (...args: Args) => SelectorConfig<T>;
 }
 
-export interface AsyncSelectorFamilyConfig<T, Args extends unknown[]> {
-  readonly key: (...args: Args) => any;
-  readonly selector: (...args: Args) => AsyncSelectorConfig<T>;
-}
-
-export interface NewSelectorFamily {
-  <T, Args extends unknown[]>(config: SelectorFamilyConfig<T, Args>): Family<Selector<T>, Args>;
-  async<T, Args extends unknown[]>(
-    config: AsyncSelectorFamilyConfig<T, Args>,
-  ): Family<AsyncSelector<T>, Args>;
-}
-
-const createSelectorFamilyCreator = () => {
-  const selectorFamily = (<T, Args extends unknown[]>(
-    config: SelectorFamilyConfig<T, Args>,
-  ): Family<Selector<T>, Args> => {
-    const family = new Family({
-      key: config.key,
-      value: (...args) => {
-        const selectorConfig = config.selector(...args);
-        return selector({
-          ...selectorConfig,
-          onCacheInvalidate: selectorConfig.onCacheInvalidate,
-          onDelete: (event) => {
-            selectorConfig.onDelete && selectorConfig.onDelete(event);
-            family.delete(...args);
-          },
-        });
-      },
-    });
-    return family;
-  }) as NewSelectorFamily;
-
-  const asyncSelectorFamily = <T, Args extends any[]>(
-    config: AsyncSelectorFamilyConfig<T, Args>,
-  ): Family<AsyncSelector<T>, Args> => {
-    const family = new Family({
-      key: config.key,
-      value: (...args) => {
-        const selectorConfig = config.selector(...args);
-        return selector.async({
-          ...selectorConfig,
-          onCacheInvalidate: selectorConfig.onCacheInvalidate,
-          onDelete: (event) => {
-            selectorConfig.onDelete && selectorConfig.onDelete(event);
-            family.delete(...args);
-          },
-        });
-      },
-    });
-    return family;
-  };
-
-  selectorFamily.async = asyncSelectorFamily;
-  return selectorFamily;
+export const selectorFamily = <T, Args extends unknown[]>(
+  config: SelectorFamilyConfig<T, Args>,
+): Family<Selector<T>, Args> => {
+  const family = new Family({
+    key: config.key,
+    value: (...args) => {
+      const selectorConfig = config.selector(...args);
+      return selector({
+        ...selectorConfig,
+        onCacheInvalidate: selectorConfig.onCacheInvalidate,
+        onDelete: (event) => {
+          selectorConfig.onDelete && selectorConfig.onDelete(event);
+          family.delete(...args);
+        },
+      });
+    },
+  });
+  return family;
 };
-
-export const selectorFamily = createSelectorFamilyCreator();
