9ee6f6cb930dcf651af35177ecfbded727d25ea3
ryym <ryym.64@gmail.com>
2022-07-17 20:37:42 +0900

    refactor: Delete less meaningful tests

--- src/__tests__/block.test.ts
+++
@@ -1,8 +0,0 @@
-import { block } from "../block";
-
-describe("Block", () => {
-  it("has a default value", () => {
-    const count = block({ default: () => 0 });
-    expect(count.default()).toEqual(0);
-  });
-});
