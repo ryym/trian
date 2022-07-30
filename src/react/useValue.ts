99d26328898b074edf524811a68f141006f8a4a3
ryym <ryym.64@gmail.com>
2022-07-30 16:34:28 +0900

    feat(useValue): Use useSyncExternalStore for Reac18

--- src/react/useValue.ts
+++ src/react/useValue.ts
@@ -1,23 +1,17 @@
-import { useState, useEffect } from "react";
+import { useSyncExternalStore, useCallback } from "react";
 import { useTrianContext } from "./TrianContext";
 import { Block } from "../block";
 import { Selector } from "../selector";
 
-export const useValue = <T>(passedKey: Block<T> | Selector<T>): T => {
+export const useValue = <T>(key: Block<T> | Selector<T>): T => {
   const { store } = useTrianContext();
-  const [key] = useState(passedKey);
-
-  const [value, setValue] = useState(store.getValue(key));
-
-  useEffect(() => {
-    const unsubscribe = store.onInvalidate(key, (event) => {
-      const value = store.getValue(key);
-      if (event.last == null || event.last.value !== value) {
-        setValue(value);
-      }
-    });
-    return unsubscribe;
-  }, []);
-
-  return value;
+  return useSyncExternalStore(
+    useCallback(
+      (callback) => {
+        return store.onInvalidate(key, callback);
+      },
+      [store, key],
+    ),
+    useCallback(() => store.getValue(key), [store, key]),
+  );
 };
